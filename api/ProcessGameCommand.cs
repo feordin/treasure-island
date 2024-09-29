5921cb46f96a2d201d780371469fb90eb1f357ad
feordin <jaerwin@microsoft.com>
2024-09-28 20:47:43 -0700

    Update Cosmos connection

--- api/ProcessGameCommand.cs
+++ api/ProcessGameCommand.cs
@@ -2,12 +2,36 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
+using Microsoft.Azure.Cosmos;
+using Azure.Identity;
 
 namespace Erwin.Games.TreasureIsland
 {
     public class ProcessGameCommand
     {
         private readonly ILogger<ProcessGameCommand> _logger;
+        private static Lazy<CosmosClient> _lazyClient = new Lazy<CosmosClient>(InitializeCosmosClient);
+        private static CosmosClient _cosmosClient => _lazyClient.Value;
+        private static readonly string _databaseId = "treasureisland";
+        private static readonly string _containerId = "gamedata";
+
+        private static CosmosClient InitializeCosmosClient()
+        {
+            // Perform any initialization here
+            var uri = Environment.GetEnvironmentVariable("CosmosDBEndpoint");
+            var isLocal = Environment.GetEnvironmentVariable("AZURE_FUNCTIONS_ENVIRONMENT") == "Development";
+
+            if (isLocal)
+            {
+                var emulatorKey = Environment.GetEnvironmentVariable("CosmosDBEmulatorKey");
+                return new CosmosClient(uri, emulatorKey);
+            }
+            else
+            {
+                var credential = new DefaultAzureCredential();
+                return new CosmosClient(uri, credential);
+            }
+        }
 
         public ProcessGameCommand(ILogger<ProcessGameCommand> logger)
         {
@@ -15,10 +39,16 @@ namespace Erwin.Games.TreasureIsland
         }
 
         [Function("ProcessGameCommand")]
-        public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+        public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
-            return new OkObjectResult("Welcome to Azure Functions!");
+
+            var container = _cosmosClient.GetContainer(_databaseId, _containerId);
+            var response = await container.ReadItemAsync<dynamic>("startinglocationdescription", new PartitionKey("startinglocationdescription"));
+            var document = response.Resource;
+            var returnValue = ((Newtonsoft.Json.Linq.JObject)document).Value<string>("description");
+            
+            return new OkObjectResult(returnValue);
         }
     }
 }
